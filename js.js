3de8265414ed3d1dbdcd3150cd925c4363c9e68d
Milos Grabovcic <miksergrabo@gmail.com>
2022-08-28 19:35:52 +0200

    Update js.js

--- js.js
+++ js.js
@@ -1,6 +1,6 @@
-function start() {
-    alert('this website is a work in progress!');
-}
+
+alert('this website is a work in progress!');
+
 function send() {
     let msg = document.querySelector('.msg');
     msg.innerHTML = '<span>Processing email request... <br> You will be redirected soon.</span>';
